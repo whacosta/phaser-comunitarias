3a35d6a44728ff556a465ce60875b91c8c79a364
Wilmer Acosta <whacosta@espol.edu.ec>
2018-01-03 22:33:39 -0500

    Cambio dimensiones

--- src/MenuState.ts
+++ src/MenuState.ts
@@ -10,23 +10,80 @@ class MenuState extends Phaser.State {
 
     const menu = this.game.add.button(
       ancho / 2,
-      alto / 2 + 80,
+      alto / 2,
       'menu',
       this.playTheGame,
       this
     );
     menu.anchor.setTo(0.5, 0.5);
+    menu.scale.setTo(0.9, 0.8);
 
-    this.navbar = new Phaser.Rectangle(0, alto - 50, ancho, 50);
+    //this.navbar = new Phaser.Rectangle(0, alto - 60, ancho, 60);
+    const btnAvatar = this.game.add.button(
+      26,
+      alto - 120,
+      'avatar',
+      this.btnAvatarAction,
+      this
+    );
+    const btnChancho = this.game.add.button(
+      235,
+      alto - 120,
+      'chancho',
+      this.btnChanchoAction,
+      this
+    );
+    const btnCofre = this.game.add.button(
+      470,
+      alto - 120,
+      'cofre',
+      this.btnCofreAction,
+      this
+    );
+    const btnTienda = this.game.add.button(
+      705,
+      alto - 120,
+      'tienda',
+      this.btnTiendaAction,
+      this
+    );
+    const btnChat = this.game.add.button(
+      940,
+      alto - 120,
+      'chat',
+      this.btnChatAction,
+      this
+    );
+    //const btnAhorros = this.game.add.button( );
   }
 
   render() {
-    this.game.debug.geom(this.navbar, '#0404B4');
+    //this.game.debug.geom(this.navbar, '#0404B4');
   }
 
   playTheGame() {
     this.game.state.add('TheGame', new GameState());
     this.game.state.start('TheGame');
+  }
+
+  btnAvatarAction() {
+    console.log('Press Avatar');
+  }
+
+  btnChanchoAction() {
+    console.log('Press chancho');
+  }
+
+  btnCofreAction() {
+    console.log('Press cofre');
+  }
+
+  btnTiendaAction() {
+    console.log('Press tienda');
+  }
+
+  btnChatAction() {
+    console.log('Press chat');
   }
 }
 
