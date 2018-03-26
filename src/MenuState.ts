8a65a0ef9490ccaa19e89d71e8e8bc7b465d6975
waenriqu <wesquall@hotmail.com>
2018-03-26 10:32:00 -0500

    cambios para igualar index y menustate

--- src/MenuState.ts
+++ src/MenuState.ts
@@ -1,56 +1,124 @@
 import GameState from './GameState.ts';
 import { alto, ancho } from './dimens.ts';
+interface ButtonData {
+  resourceName: string;
+  onClick?: () => void;
+}
 
 class MenuState extends Phaser.State {
+  private addTopIcon(index: number, resourceName: string) {
+    const icon = this.game.add.sprite(
+      ancho * index / 6,
+      alto * 8 / 40,
+      resourceName
+    );
+    icon.anchor.setTo(0.5, 0.5);
+    icon.height = alto * 15 / 160;
+    icon.width = icon.height * 1.0724;
+  }
+
+  private addBottomButton(index: number, resourceName: string) {
+    const icon = this.game.add.button(
+      ancho * index / 6,
+      alto * 9 / 10,
+      resourceName
+    );
+    icon.anchor.setTo(0.5, 0.5);
+    icon.height = alto * 15 / 160;
+    icon.width = icon.height;
+  }
+
+  private addSprite(params: {
+    resourceName: string;
+    posX: number;
+    posY: number;
+    height: number;
+    widthToHeight: number;
+  }) {
+    const icon = this.game.add.sprite(
+      params.posX,
+      params.posY,
+      params.resourceName
+    );
+    icon.anchor.setTo(0.5, 0.5);
+    icon.height = params.height;
+    icon.width = icon.height * params.widthToHeight;
+  }
+
+  private addButtonColumn(columnIndex: number, buttons: ButtonData[]) {
+    const buttonHeight = alto * 4 / 32;
+    const widthToHeight = 1881 / 1304;
+    const firstYPos = alto * 13 / 40;
+    buttons.forEach((buttonData, rowIndex) => {
+      const button = this.game.add.button(
+        ancho * columnIndex / 6,
+        firstYPos + rowIndex * buttonHeight,
+        buttonData.resourceName,
+        buttonData.onClick
+      );
+      button.anchor.setTo(0.5, 0.5);
+      button.height = buttonHeight;
+      button.width = button.height * widthToHeight;
+    });
+  }
+
+  private startGame() {
+    this.game.state.start('Casas');
+  }
+
   create() {
-    const gameTitle = this.game.add.sprite(ancho / 2, 50, 'logo');
-    gameTitle.anchor.setTo(0.5, 0.5);
+    this.addSprite({
+      resourceName: 'logo',
+      posX: ancho / 2,
+      posY: alto / 10,
+      height: alto * 15 / 160,
+      widthToHeight: 3.805,
+    });
 
-    const menu = this.game.add.button(
-      ancho / 2,
-      alto / 2,
-      'menu',
-      this.playTheGame,
-      this
-    );
-    menu.anchor.setTo(0.5, 0.5);
-    menu.scale.setTo(0.9, 0.8);
+    this.addTopIcon(1, 'cepillo_icon');
+    this.addTopIcon(2, 'doctor_icon');
+    this.addTopIcon(3, 'libro_icon');
+    this.addTopIcon(4, 'mujer_hombre_icon');
+    this.addTopIcon(5, 'manos_icon');
 
-    const btnAvatar = this.game.add.button(
-      26,
-      alto - 120,
-      'avatar',
-      this.btnAvatarAction,
-      this
-    );
-    const btnChancho = this.game.add.button(
-      235,
-      alto - 120,
-      'chancho',
-      this.btnChanchoAction,
-      this
-    );
-    const btnCofre = this.game.add.button(
-      470,
-      alto - 120,
-      'cofre',
-      this.btnCofreAction,
-      this
-    );
-    const btnTienda = this.game.add.button(
-      705,
-      alto - 120,
-      'tienda',
-      this.btnTiendaAction,
-      this
-    );
-    const btnChat = this.game.add.button(
-      940,
-      alto - 120,
-      'chat',
-      this.btnChatAction,
-      this
-    );
+    this.addButtonColumn(1, [
+      { resourceName: 'hogares1_button' },
+      { resourceName: 'hogares1_button' },
+      { resourceName: 'salud_oral_button' },
+    ]);
+
+    this.addButtonColumn(2, [
+      { resourceName: 'serv_medicos_button' },
+      { resourceName: 'serv_odont_button' },
+      { resourceName: 'rehab_nutr_button' },
+    ]);
+
+    this.addButtonColumn(3, [
+      { resourceName: 'refuerzo_ninos_button' },
+      { resourceName: 'refuerzo_jovenes_button' },
+      { resourceName: 'curso_comp_button' },
+      { resourceName: 'centro_descu_button' },
+    ]);
+
+    this.addButtonColumn(4, [
+      { resourceName: 'aflatoun_button', onClick: this.startGame.bind(this) },
+      { resourceName: 'a_jugar_button' },
+      { resourceName: 'consejo_jovenes_button' },
+      { resourceName: 'empoderarte_button' },
+    ]);
+
+    this.addButtonColumn(5, [
+      { resourceName: 'hacia_empleo_button' },
+      { resourceName: 'construyendo_button' },
+      { resourceName: 'becas_button' },
+      { resourceName: 'emprendiendo_button' },
+    ]);
+
+    this.addBottomButton(1, 'avatar');
+    this.addBottomButton(2, 'chancho');
+    this.addBottomButton(3, 'cofre');
+    this.addBottomButton(4, 'tienda');
+    this.addBottomButton(5, 'chat');
   }
 
   render() {}
--- src/index.ts
+++ src/index.ts
@@ -3,29 +3,30 @@ import 'pixi.js';
 import 'p2';
 import 'phaser';
 
+import { ancho, alto } from './dimens.ts';
 import BootState from './BootState.ts';
 import VideoState from './VideoState.ts';
 import GameState from './GameState.ts';
 import BoyGirlState from './BoyGirlState.ts';
 import LoadState from './LoadState.ts';
 import LoginState from './LoginState.ts';
+import CasasState from './CasasState.ts';
 import EncuentraAflatoun from './EncuentraAflatoun.ts';
 import EligeAflatoun from './EligeAflatoun.ts';
-import { ancho, alto } from './dimens.ts';
 import AflatounAstronauta from './AflatounAstronauta.ts';
 import DefinisteTuSueno from './DefinisteTuSueno.ts';
 import ProfesionesArrastrables from './ProfesionesArrastrables.js';
 import ProfesionesPintables from './ProfesionesPintables.js';
+import LosValores1 from './LosValores1.js';
+import ComoMeAlimento from './ComoMeAlimento.js';
+import IdentificandoAflatoun from './IdentificandoAflatoun.js';
+import CompletarAflatoun2 from './CompletarAflatoun2.js';
+import CompletarAflatoun1 from './CompletarAflatoun1.js';
 
 import Video1 from './assets/video1.mp4';
 
 function start() {
-  const game = new Phaser.Game(
-    window.innerWidth,
-    window.innerHeight,
-    Phaser.AUTO,
-    'game'
-  );
+  const game = new Phaser.Game(ancho, alto, Phaser.AUTO, 'game');
 
   game.state.add('Login', new LoginState());
   game.state.add('Boot', new BootState());
@@ -36,8 +37,14 @@ function start() {
   game.state.add('AflatounAstronauta', new AflatounAstronauta());
   game.state.add('ProfesionesArrastrables', ProfesionesArrastrables);
   game.state.add('ProfesionesPintables', ProfesionesPintables);
+  game.state.add('LosValores1', LosValores1);
+  game.state.add('ComoMeAlimento', ComoMeAlimento);
+  game.state.add('IdentificandoAflatoun', IdentificandoAflatoun);
+  game.state.add('CompletarAflatoun2', CompletarAflatoun2);
+  game.state.add('CompletarAflatoun1', CompletarAflatoun1);
   game.state.add('DefinisteTuSueno', DefinisteTuSueno);
-  game.state.start('Boot');
+  game.state.add('Casas', CasasState);
+  game.state.start('Login');
 }
 
 start();
